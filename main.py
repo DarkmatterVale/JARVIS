f4d18637158b06c15b7bc9826fefadd16c922596
Vale <valetolpegin@gmail.com>
2015-05-19 07:08:13 -0400

    Added comments for objectives in this class

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import sys
 import subprocess
 import os
 
-input_var = raw_input( "ENTER VALUE: " )
-print input_var
+# NOTE: Currently, only CLI will be implemented so STT engines and other class initialization will not be required
+
+# Throughout the code in the main program, the following needs to be accomplished:
+# 1: Initialize brain and other classes. This will allow JARVIS to interpret commands and access STT associated functions
+# 2: In an infinite loop:
+#   a: Get user input
+#   b: Parse that user input for set commands such as shutdown, restart, etc
+#   c: Compare that input to previous input/database of phrases and words and apply learning logic (see logic_basic_learning.txt)
