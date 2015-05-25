64ccabab5825d053f1f60d72cea4e8ebe6700fdd
Vale <valetolpegin@gmail.com>
2015-05-24 21:29:11 -0400

    Major brain update
    - Allows for more dynamic command recognition
         - This is completed using the json python library

--- brain.py
+++ brain.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import os
 import json
+import re
 
 # This program is the main brain of JARVIS
 # This is extended into submodules in the following ways:
@@ -28,6 +29,7 @@ class brain:
         # TODO: convert hardcoded gretting to something more dynamic
         if user_gone:
             print "Hello again sir"
+            
             user_gone = False
             return
         
@@ -40,12 +42,49 @@ class brain:
         # TODO: convert hardcoded commands into dictionary synonyms of "bye"
         if 'bye' in command or 'see ya later' in command:
             print "Ok, bye sir"
-            user_gone = True
             
+            user_gone = True
             return
+        
+        try:
+            data = json.load( open( 'learning/phrase_list.json' ) )
+            print data[ command ][ 'response' ]
+        
+            return
+        except:
+            pass
+        
+        # Otherwise, since the command is not recognized, add it to the list of recognzed command
+        print "Sorry sir, but I am still learning. I will save this phrase and learn it as time goes on. Is there a specific response you would like me to reply with?"
+        save_phrase = raw_input( "COMMAND: " )
+        if save_phrase != 'no':
+            # Opening file, to read and then to write
+            output = {}
+            try:
+                output = json.load( open( 'learning/phrase_list.json' ) )
+            except:
+                pass
+            
+            file = open( 'learning/phrase_list.json', 'w' )
+            
+            # Creating the entry for the file
+            output[ command ] = { "response" : save_phrase }
+            
+            # Putting the entry in the file
+            json.dump( output, file )
 
-        print "Sorry sir, but I am still learning. I will save this phrase and learn it as time goes on"
+            # Closing the file since all I/O operations have been completed
+            file.close()
 
+            # Telling the user the phrase has been saved
+            print "The phrase has been saved"
+
+    # This method will find a phrase that is synonymous to the incoming phrase, making it look like JARVIS has some random behavior
+    # TODO: add synonymous phrase finding code below
+    def find_synonymous_phrase( self, phrase ):
+        pass
+
+# If this is run as a standalone program, tell the user that it is not meant to be that way
 if __name__ == '__main__':
     print ""
     print "THIS IS NOT MEANT TO BE RUN AS A STANDALONE PROGRAM...PLEASE RUN 'python main.py' to run JARVIS"
