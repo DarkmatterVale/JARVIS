90f2d7b0f3b4c1cd53253f10bda3591b633d4656
Vale <valetolpegin@gmail.com>
2016-02-04 10:16:01 -0500

    Fix require name

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     packages = find_packages(),
 
     platforms=["any"],
-    install_requires = ['chatterbot'],
+    install_requires = ['ChatterBot'],
 
     keywords = [ 'NLP', 'assistant', 'intelligent', 'AI' ],
 )
