361bff36db6b1a46e7593e44cd03ddaec7858625
Vale <valetolpegin@gmail.com>
2016-02-04 09:07:53 -0500

    Remove Django project -- It needs to be put into a separate repository

--- JARVIS/website/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-class Answer(models.Model):
-    answer_text = models.CharField(max_length=200)
-    answer_date = models.DateTimeField('date answered')
