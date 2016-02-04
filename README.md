7342939935761b028834231edcbb52ad030aaa2f
Vale <valetolpegin@gmail.com>
2016-02-04 09:13:44 -0500

    A little cleanup and update documentation

--- README.md
+++ README.md
@@ -8,21 +8,13 @@ python jarvis.py
 
 Please report any bugs or feature requests here on the Github issue tracker.
 
-## Dependencies included in project
+## Installation
 
-This project includes a number of dependencies. I have not developed these, and do not take any credit for them. Please read specific information about each library. Currently included libraries:
+To install JARVIS, use pip.
 
-- None
-
-Once again, I do not take any credit for these.
-
-## Current Status
-
-I have postoned working on this project since I have been working on ChatterBot. Hopefully, much of this project's functionality can be built into chatterbot as extensions.
-
-## Building
-
-No building should be required, so you do not need to worry about this. All you need to do is type the above command into terminal to start JARVIS, and voila! Your very own personal assistant!
+```
+pip install JARVIS
+```
 
 ## Contributing
 
--- jarvis.py
+++ jarvis.py
@@ -1,39 +1,35 @@
-""" Main JARVIS class """
-
 from core import *
 
 
 class Jarvis:
-
-    def __init__( self, *args, **kwargs ):
-        """ Blank constructor method """
-
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor method
+        """
         self.jarvis_brain = brain()
 
-
-    def run_jarvis( self ):
-        """ Main method.....Interfaces with Jarvis' brain and displays responses """
-
+    def run_jarvis(self):
+        """
+        Interfaces with Jarvis' brain and displays responses
+        """
         while True:
-            user_input = raw_input( "Human: " )
+            user_input = raw_input("Human: ")
 
             # Leave if the user is done
             if user_input == "quit":
-                exit( 0 )
+                exit(0)
 
             # Generate response
-            response = self.jarvis_brain.generate_response( user_input )
+            response = self.jarvis_brain.generate_response(user_input)
 
             # Print response
             print response
 
 
-    def get_response( self, user_input ):
+    def get_response(self, user_input):
         """
-        Returns the response generated from
-        the passed user_input variable
+        Returns the response generated from the passed user_input variable
         """
-
         return self.jarvis_brain.generate_response( user_input )
 
 
